f9772a482b3cc5521ce0b979b1080b3e59aefc87
julianooii <julianooi80@gmail.com>
2023-10-23 01:33:27 +0800

    updated to take query params

--- src/main/java/com/backend/configuration/Constants.java
+++ src/main/java/com/backend/configuration/Constants.java
@@ -12,6 +12,6 @@ public final class Constants {
     public static final String MESSAGE_SAMEEMAILEXIST = "User with the same email exists.";
     public static final String MESSAGE_MISSINGPORTFOLIONAME = "Portfolio name is missing.";
     public static final String MESSAGE_MISSINGPORTFOLIODESC = "Portfolio description is missing.";
-    public static final String MESSAGE_INVALIDHISTORICALCALL = "Incorrect GET request. Appropriate parameters are 1week, 1month, 6month, 1year and 5year.";
+    public static final String MESSAGE_INVALIDHISTORICALCALL = "Incorrect request. Appropriate query parameters are 1week, 1month, 6month, 1year, 5year and all";
 
 }
--- src/main/java/com/backend/controller/AssetRefDataController.java
+++ src/main/java/com/backend/controller/AssetRefDataController.java
@@ -8,10 +8,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.backend.model.AssetRefData;
@@ -31,19 +30,19 @@ public class AssetRefDataController {
         this.assetRefDataService = assetRefDataService;
     }
 
-	@GetMapping(path = "/asset/{assetId}")
-    public AssetRefDataResponse getAssetRefData(@PathVariable long assetId){
-        List<AssetRefData> assetList = assetRefDataService.findAllByAssetId(assetId);
-        AssetRefDataResponse response = new AssetRefDataResponse();
-        response.setAssetRefDataList(assetList);
-        return response;
-    }
+	// @GetMapping(path = "/asset/info/{assetId}")
+    // public AssetRefDataResponse getAssetRefData(@PathVariable long assetId){
+    //     List<AssetRefData> assetList = assetRefDataService.findAllByAssetId(assetId);
+    //     AssetRefDataResponse response = new AssetRefDataResponse();
+    //     response.setAssetRefDataList(assetList);
+    //     return response;
+    // }
 
-    @GetMapping(path = "/asset/{assetId}/{duration}")
-    public AssetRefDataResponse getHistoricalAssetRefData(@PathVariable("assetId") long assetId, @PathVariable("duration") String duration){
+    @GetMapping(path = "/asset/{assetId}")
+    public AssetRefDataResponse getHistoricalAssetRefData(@PathVariable("assetId") long assetId, @RequestParam("duration") String duration){
         LocalDate endDate = LocalDate.now().minusDays(1);
         LocalDate startDate;
-        
+        System.out.println(duration);
         switch (duration){
             case "1week":
                 startDate = endDate.minusWeeks(1).plusDays(1);
@@ -60,6 +59,13 @@ public class AssetRefDataController {
             case "5year":
                 startDate = endDate.minusYears(5).plusDays(1);
                 break;
+            case "all":
+                System.out.println("Collecting ALL asset reference data...");
+                List<AssetRefData> assetList = assetRefDataService.findAllByAssetId(assetId);
+                AssetRefDataResponse response = new AssetRefDataResponse();
+                response.setAssetRefDataList(assetList);
+                System.out.println("ALL asset reference data collected.");
+                return response;
             default:
                 startDate = null;
         }
