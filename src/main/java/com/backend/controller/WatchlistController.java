29e9a1bcb9bca574ec384cfd1b490a9a79cce4f0
Julian Ooi <101630007+julianooii@users.noreply.github.com>
2023-11-08 22:51:53 +0800

    feat: API endpoint to fetch prices of all assets in watchlist

--- src/main/java/com/backend/controller/WatchlistController.java
+++ src/main/java/com/backend/controller/WatchlistController.java
@@ -1,32 +1,44 @@
 package com.backend.controller;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import java.util.List;
 
-// import com.backend.exception.PortfolioAssetNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 import com.backend.configuration.Constants;
+import com.backend.configuration.Environment;
 import com.backend.exception.BadRequestException;
-// import com.backend.exception.WatchlistNotFoundException;
-// import com.backend.model.Portfolio;
-// import com.backend.model.Portfolio;
-// import com.backend.model.Asset;
 import com.backend.model.Watchlist;
 import com.backend.service.abstractions.IWatchlistService;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import com.backend.request.CreateWatchlistRequest;
-// import com.backend.request.UpdatePortfolioMetaDataRquest;
 import com.backend.response.CreateWatchlistResponse;
 import com.backend.response.GetWatchlistByIdResponse;
-// import com.backend.response.UpdatePortfolioMetadataResponse;
+import com.backend.response.GetWatchlistPriceDataResponse;
 import com.backend.request.UpdateWatchlistAssetsRequest;
 import com.backend.response.UpdateWatchlistAssetsResponse;
 import com.backend.request.RemoveWatchlistAssetRequest;
 import com.backend.response.RemoveWatchlistAssetResponse;
-
-// import java.util.List;
+import com.backend.exception.WatchlistNotFoundException;
 
 @RestController
 @RequestMapping(produces = { MediaType.APPLICATION_JSON_VALUE })
@@ -39,27 +51,109 @@ public class WatchlistController {
         this.watchlistService = watchlistService;
     }
 
+	public static Map<String, Object> getAssetDataOverview(String assetTicker, String apikey) throws Exception{
+		Map<String, Object> output = new HashMap<>();
+		// for overview -> MarketCapitalization and name
+		assetTicker = assetTicker.replaceAll("\\s", "");
+		String apiUrl = "https://www.alphavantage.co/query?function=OVERVIEW&symbol=" + assetTicker
+				+ "&apikey=" + apikey;
+		HttpGet httpGet = new HttpGet(apiUrl);
+		HttpClient httpClient = HttpClients.createDefault();
+        HttpResponse response = httpClient.execute(httpGet);
+
+        // Extract the JSON response as a String
+        String jsonResponse = EntityUtils.toString(response.getEntity());
+
+        Gson gson = new GsonBuilder().create();
+        JsonObject jsonRes = gson.fromJson(jsonResponse, JsonObject.class);
+        String assetName = jsonRes.getAsJsonPrimitive("Name").getAsString();
+		long mktCap = jsonRes.getAsJsonPrimitive("MarketCapitalization").getAsLong();
+		// put assetName and mktCap into a temp map to put into the output
+		output.put("AssetOverview Name", assetName);
+		output.put("AssetOverview MarketCapitalization", mktCap);
+
+		return output;
+	}
+
+	public static Map<String, Map<String, Object>> getAssetDataIntraday(String assetTicker, String apikey) throws Exception{
+		Map<String, Map<String, Object>> output = new HashMap<>();
+		Map<String, Object> tempMap = new HashMap<>();
+
+		// for hourly -> top 168 --> adjusted close
+		assetTicker = assetTicker.replaceAll("\\s", "");
+		String apiUrl = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol="
+				+ assetTicker + "&interval=60min&outputsize=full&apikey=" + apikey;
+		HttpGet httpGet = new HttpGet(apiUrl);
+		HttpClient httpClient = HttpClients.createDefault();
+        HttpResponse response = httpClient.execute(httpGet);
+
+        // Extract the JSON response as a String
+        String jsonResponse = EntityUtils.toString(response.getEntity());
+
+        Gson gson = new GsonBuilder().create();
+        JsonObject jsonRes = gson.fromJson(jsonResponse, JsonObject.class);
+        JsonObject assetHistPrices = jsonRes.getAsJsonObject("Time Series (60min)");
+
+		int count = 0; 
+		for (String dateStr : assetHistPrices.keySet()){
+			if (count == 168){ // 168 hours in 7 days
+				break;
+			}
+			JsonObject priceJson = assetHistPrices.getAsJsonObject(dateStr);
+			double adjClosePrice = priceJson.getAsJsonPrimitive("4. close").getAsDouble();
+			tempMap.put(dateStr, adjClosePrice);
+			count += 1;
+		}
+		output.put("AssetIntraday", tempMap);
+		
+		return output;
+	}
+
+	public static Map<String, Object> getAssetDataGlobalQuote(String assetTicker, String apikey) throws Exception {
+		Map<String, Object> output = new HashMap<>();
+		// for global quote -> 5. and 6.
+		assetTicker = assetTicker.replaceAll("\\s", "");
+		String apiUrl = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=" + assetTicker
+				+ "&apikey=" + apikey;
+		HttpGet httpGet = new HttpGet(apiUrl);
+		HttpClient httpClient = HttpClients.createDefault();
+        HttpResponse response = httpClient.execute(httpGet);
+
+        // Extract the JSON response as a String
+        String jsonResponse = EntityUtils.toString(response.getEntity());
+
+        Gson gson = new GsonBuilder().create();
+        JsonObject jsonRes = gson.fromJson(jsonResponse, JsonObject.class);
+		JsonObject assetGlobalQuote = jsonRes.getAsJsonObject("Global Quote");
+        double price = assetGlobalQuote.getAsJsonPrimitive("05. price").getAsDouble();
+        long volume = assetGlobalQuote.getAsJsonPrimitive("06. volume").getAsLong();
+		output.put("Global Quote Price", price);
+		output.put("Global Quote Volume", volume);
+
+		return output;
+	}
+
     @PostMapping(path = "/watchlist")
     public CreateWatchlistResponse createWatchlist(@RequestBody CreateWatchlistRequest request) {
 
-		if (request.getWatchlist_assets() == null || request.getUserId()==null || request.getUserId().isEmpty()) {
+		if (request.getWatchlistAssets() == null || request.getUserId()==null || request.getUserId().isEmpty()) {
 			throw new BadRequestException(Constants.MESSAGE_MISSINGWATCHLISTDATA);
 		}
 
 		logger.info("Beginning creation of new watchlist with the following details");
 		logger.info("Watchlist User ID: " + request.getUserId());
-		logger.info("New Watchlist Assets: " + request.getWatchlist_assets());
+		logger.info("New Watchlist Assets: " + request.getWatchlistAssets());
 		Watchlist watchlist = watchlistService.createWatchlist(
 				new Watchlist(
 						request.getUserId(),
-						request.getWatchlist_assets()
+						request.getWatchlistAssets()
                 )
         );
 
 		CreateWatchlistResponse response = new CreateWatchlistResponse();
 		response.setWatchlistId(watchlist.getWid());
 		response.setUserId(watchlist.getUid());
-		response.setWatchlist_assets(watchlist.getAssets());
+		response.setWatchlistAssets(watchlist.getAssets());
 
 		return response;
     }
@@ -71,32 +165,59 @@ public class WatchlistController {
 
     @GetMapping(path = "/watchlist/wid/{wid}")
     public GetWatchlistByIdResponse getWatchlistByWid(@PathVariable long wid) {
-    Watchlist watchlist = watchlistService.findByWid(wid);
+		Watchlist watchlist = watchlistService.findByWid(wid);
 
-	// if (watchlist == null) {
-	// 		throw new WatchlistNotFoundException(wid);
-	// 	}
-    GetWatchlistByIdResponse response = new GetWatchlistByIdResponse();
-    response.setUserId(watchlist.getUid());
-    response.setWatchlistId(watchlist.getWid());
-    response.setWatchlist_assets(watchlist.getAssets());
+		// if (watchlist == null) {
+		// 		throw new WatchlistNotFoundException(wid);
+		// 	}
+		GetWatchlistByIdResponse response = new GetWatchlistByIdResponse();
+		response.setUserId(watchlist.getUid());
+		response.setWatchlistId(watchlist.getWid());
+		response.setWatchlist_assets(watchlist.getAssets());
 
-    return response;
+		return response;
     }
 
 	@GetMapping(path = "/watchlist/user/{uid}")
-    public GetWatchlistByIdResponse getWatchlistByUid(@PathVariable String uid) {
-    Watchlist watchlist = watchlistService.findByUid(uid);
+    public GetWatchlistPriceDataResponse getWatchlistPricesByUid(@PathVariable String uid) throws Exception {
+		Watchlist watchlist = watchlistService.findByUid(uid);
 
-	// if (watchlist == null) {
-	// 		throw new WatchlistNotFoundException(wid);
-	// 	}
-    GetWatchlistByIdResponse response = new GetWatchlistByIdResponse();
-    response.setUserId(watchlist.getUid());
-    response.setWatchlistId(watchlist.getWid());
-    response.setWatchlist_assets(watchlist.getAssets());
+		// if (watchlist == null) {
+		// 		throw new WatchlistNotFoundException(wid);
+		// }
 
-    return response;
+		List<String> assets = watchlist.getAssets();
+		
+		List<Map<String, Object>> output = new ArrayList<>();
+	
+		ExecutorService executor = Executors.newFixedThreadPool(30);
+		List<Callable<Map<String, Object>>> overviewTasks = new ArrayList<>();
+
+		for (String assetTicker : assets) {
+			overviewTasks.add(() -> {
+				Map<String, Object> assetNameData = getAssetDataOverview(assetTicker, Environment.getEnv("APIKEY"));
+				Map<String, Map<String,Object>> intradayData = getAssetDataIntraday(assetTicker, Environment.getEnv("APIKEY"));
+				Map<String, Object> globalQuoteData = getAssetDataGlobalQuote(assetTicker, Environment.getEnv("APIKEY"));
+				Map<String, Object> priceData = new HashMap<>();
+				Map<String, Object> tempMap = new HashMap<>();
+				tempMap.putAll(assetNameData);
+				tempMap.putAll(intradayData);
+				tempMap.putAll(globalQuoteData);
+				priceData.put(assetTicker, tempMap);
+				return priceData;
+			});
+		}
+		List<Future<Map<String, Object>>> futures = executor.invokeAll(overviewTasks);
+		for (Future<Map<String, Object>> future : futures) {
+			Map<String, Object> priceData = future.get();
+			output.add(priceData);
+		}
+		
+
+		GetWatchlistPriceDataResponse response = new GetWatchlistPriceDataResponse();
+		response.setWatchlistAssetPriceMap(output);
+
+		return response;
     }
 
 
