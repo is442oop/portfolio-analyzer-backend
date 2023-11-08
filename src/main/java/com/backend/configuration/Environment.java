fe01c9f72e71b58c6a359ee13331c264f4d634ee
David Lin <13061926+DavidLHW@users.noreply.github.com>
2023-11-09 02:19:33 +0800

    fix: default env

--- src/main/java/com/backend/configuration/Environment.java
+++ src/main/java/com/backend/configuration/Environment.java
@@ -3,10 +3,13 @@ package com.backend.configuration;
 import io.github.cdimascio.dotenv.Dotenv;
 
 public class Environment {
-    private static final Dotenv dotenv = Dotenv.configure().load();
+    private static final Dotenv dotenv = Dotenv.configure().ignoreIfMalformed().ignoreIfMissing().load();
 
     public static String getEnv(String key) {
-        String k = dotenv.get(key, System.getenv(key));
+        String k = System.getenv(key);
+        if (dotenv != null) {
+            k = dotenv.get(key);
+        }
         if (k == null) {
             throw new RuntimeException("Environment variable '" + key + "' not set!");
         }
